dfce885527b011fc0ac100a4c65c9fa445467ec2
gavinchudson <gavinchudson@gmail.com>
2022-11-16 20:56:18 -0800

    Work on profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,14 +3,39 @@ import { UserAuth } from '../context/AuthContext';
 
 const Profile = () => {
   const { user } = UserAuth();
+  const [open, setOpen] = React.useState(false);
 
+  const handleOpen = () => {
+    setOpen(!open);
+  };
 
   return (
     <div className='w-[300px] m-auto'>
-      <h1 className='text-center text-2xl font-bold pt-12'>Profile</h1>
+      <h1 className='text-center text-2xl font-bold pt-12'>Your Profile</h1>
       <div>
         <p>Welcome, {user?.displayName}</p>
       </div>
+      <img src={'images/default_profile_pic.jpg'}></img>
+      <h1 className='text-center text-2xl font-bold pt-12'>Personal Details</h1>
+      <div>
+        <p>Height:</p>
+        <p>Weight:</p>
+      </div>
+      <h1 className='text-center text-2xl font-bold pt-12'>Interests</h1>
+      <div>
+      <button onClick={handleOpen}>Show All Interests</button>
+      {open ? (
+        <ul className="menu">
+          <li className="menu-item">
+            <button>Interest 1</button>
+          </li>
+          <li className="menu-item">
+            <button>Interest 2</button>
+          </li>
+        </ul>
+      ) : null}
+    </div>
+      <a href="/EditProfile">Edit Profile</a>
     </div>
   );
 };
