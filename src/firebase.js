31b3021bf650a71ce8f2522629aa8a02c735959e
gavinchudson <gavinchudson@gmail.com>
2022-11-28 19:09:16 -0800

    change database

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA_xpVg0Dv25waK125wjp50ANTyggNO4n0",
-  authDomain: "broncofit-366907.firebaseapp.com",
-  projectId: "broncofit-366907",
-  storageBucket: "broncofit-366907.appspot.com",
-  messagingSenderId: "1076521433967",
-  appId: "1:1076521433967:web:6efd9c718dbe37bcd8c69a",
-  measurementId: "G-1PV3KR9LRC"
+  apiKey: "AIzaSyDUn1As6gh0ZlATuCdmplbdpNrbk9s6oHg",
+  authDomain: "broncofit2.firebaseapp.com",
+  projectId: "broncofit2",
+  storageBucket: "broncofit2.appspot.com",
+  messagingSenderId: "822190385545",
+  appId: "1:822190385545:web:0d8a884c0361be8e589958",
+  measurementId: "G-YE81B4YLB2"
 };
 
 // Initialize Firebase
