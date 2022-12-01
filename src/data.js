7fc0b6ed28e05ba80568455a4f6b813914dab5b8
cushionking2 <splash.wang@gmail.com>
2022-11-30 22:29:05 -0800

    update homepage data

--- src/data.js
+++ src/data.js
@@ -1,22 +1,22 @@
 export const posts = [
     {
       id: 1,
-      user: "John Doe",
-      title: "Hit a new PR",
+      user: "Chad Johnson",
+      title: "Deadlift Tips",
       img: "images/deadlift.png",
       desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit ad optio magnam veniam nihil, quasi in architecto dignissimos rerum, soluta ipsa nobis eos mollitia eligendi adipisci possimus pariatur dolorem ea?",
     },
     {
       id: 2,
       user: "Jane Doe",
-      title: "Try this new workout split",
+      title: "Try out these squat techniques",
       img: "images/squat.png",
       desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit ad optio magnam veniam nihil, quasi in architecto dignissimos rerum, soluta ipsa nobis eos mollitia eligendi adipisci possimus pariatur dolorem ea?",
     },
     {
       id: 3,
       user: "Adam Smith",
-      title: "Just finished my routine",
+      title: "Bench form information",
       img: "images/bench.png",
       desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit ad optio magnam veniam nihil, quasi in architecto dignissimos rerum, soluta ipsa nobis eos mollitia eligendi adipisci possimus pariatur dolorem ea?",
     },
