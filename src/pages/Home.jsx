8e4008a2c9fb65b016188be6a76448d85346dd6c
adranade <aditya.ranade.2001@gmail.com>
2022-12-07 13:39:32 -0800

    updated homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,62 @@
 import React from 'react';
 import Card from '../components/Card';
 import { posts } from "../data";
+import YoutubeEmbed from "../context/YoutubeEmbed";
 
 const Home = () => {
   return (
     <div className="homeContainer">
       <div className="feedContainer">
         <h1>BroncoFit Feed</h1>
+          <h2>Recommended Workouts</h2>
+            <h3>Videos</h3>
+              <div className="Videos">
+                <h1>Warmups for you:</h1>
+                  <h1>5 min warmup</h1>
+                    <YoutubeEmbed embedId="CSrBaHX3HxQ" />
+                  <h1>10 min warmup</h1>
+                    <YoutubeEmbed embedId="awBQVJ39sKM" />
+                  <h1>15 min warmup</h1>
+                    <YoutubeEmbed embedId="E23w7vmidHE" />
+                <h1>Deadlift for you:</h1>
+                  <h1>Basics</h1>
+                    <YoutubeEmbed embedId="vRKDvt695pg" />
+                  <h1>Growth</h1>
+                    <YoutubeEmbed embedId="ytGaGIn3SjE" />
+                  <h1>Perfect your Form</h1>
+                    <YoutubeEmbed embedId="r4MzxtBKyNE" />
+                  <h1>How much weight?</h1>
+                    <YoutubeEmbed embedId="_5z6j-AE17E" />
+                <h1>Squats for you:</h1>
+                  <h1>Basics</h1>
+                    <YoutubeEmbed embedId="sPej2dr2J5s" />
+                  <h1>Squat with Dumbell</h1>
+                    <YoutubeEmbed embedId="3PRwtVpyslo" />
+                  <h1>Use a Squat Rack</h1>
+                    <YoutubeEmbed embedId="kaO7Yw1hlcM" />
+                <h1>Bench for you:</h1>
+                  <h1>Tutorial and Proper Form</h1>
+                    <YoutubeEmbed embedId="gRVjAtPip0Y" />
+                  <h1>Benching Checklist</h1>
+                    <YoutubeEmbed embedId="vthMCtgVtFw" />
+                  <h1>Growth</h1>
+                    <YoutubeEmbed embedId="vcBig73ojpE" />
+                <h1>Free Weights for you:</h1>
+                  <h1>How to use</h1>
+                    <YoutubeEmbed embedId="KUJSm4DznM8" />
+                  <h1>8 Dumbell Exercises</h1>
+                    <YoutubeEmbed embedId="y1r9toPQNkM" />
+                  <h1>Example Dumbell Workout</h1>
+                    <YoutubeEmbed embedId="y1r9toPQNkM" />
+                <h1>Cardio for you:</h1>
+                  <h1>30 min HIIT Cardio</h1>
+                    <YoutubeEmbed embedId="6uLRtubw3ws" />
+                  <h1>Cardio vs. Strength</h1>
+                    <YoutubeEmbed embedId="YvrKIQ_Tbsk" />
+            </div>
         {posts.map(post => (
           <Card key={post.id} post={post} />
         ))}
-      </div>
-      <div className="recommendationContainer">
-        Recommended users
-        <img className="pcard" src="images/default_profile_pic.jpg" alt="userphoto" />
-        <img className="pcard" src="images/default_profile_pic.jpg" alt="userphoto" />
-        <img className="pcard" src="images/default_profile_pic.jpg" alt="userphoto" />
       </div>
     </div>
   )
